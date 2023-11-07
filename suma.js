42401aacb5c96e93be32b58b568b27d0062fb53c
Wilmer Alexis Montoya <alexis.montoya616@gmail.com>
2023-11-07 10:18:49 -0500

    Calculadora - Pinillo - (07/Noviembre/2023)

--- suma.js
+++ suma.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sumar() {
     var a = parseInt(document.getElementById("a").value)
     var b = parseInt(document.getElementById("b").value)
@@ -34,41 +33,4 @@ function division() {
     var c = parseInt(a)/parseInt(b)
 
     document.getElementById('resultado').value=c
-=======
-function sumar() {
-    var a = parseInt(document.getElementById("a").value)
-    var b = parseInt(document.getElementById("b").value)
-    
-    var c = parseInt(a)+parseInt(b)
-
-    document.getElementById('resultado').value=c
-    
-}
-
-function restar() {
-    var a = parseInt(document.getElementById("a").value)
-    var b = parseInt(document.getElementById("b").value)
-    
-    var c = parseInt(a)-parseInt(b)
-
-    document.getElementById('resultado').value=c
-}
-
-function multiplicacion() {
-    var a = parseInt(document.getElementById("a").value)
-    var b = parseInt(document.getElementById("b").value)
-    
-    var c = parseInt(a)*parseInt(b)
-
-    document.getElementById('resultado').value=c
-}
-
-function division() {
-    var a = parseInt(document.getElementById("a").value)
-    var b = parseInt(document.getElementById("b").value)
-    
-    var c = parseInt(a)/parseInt(b)
-
-    document.getElementById('resultado').value=c
->>>>>>> ba31350 (Este es el mas reciente (07/nov/2023))
 }
