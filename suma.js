9cd751feb722e8e0c8c2165ebb857610a8a9ff36
Wilmer Alexis Montoya <alexis.montoya616@gmail.com>
2023-11-07 10:12:56 -0500

    Calculadora - Pinilla - (07/nov/2023)

---
+++ suma.js
@@ -0,0 +1,74 @@
+<<<<<<< HEAD
+function sumar() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)+parseInt(b)
+
+    document.getElementById('resultado').value=c
+    
+}
+
+function restar() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)-parseInt(b)
+
+    document.getElementById('resultado').value=c
+}
+
+function multiplicacion() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)*parseInt(b)
+
+    document.getElementById('resultado').value=c
+}
+
+function division() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)/parseInt(b)
+
+    document.getElementById('resultado').value=c
+=======
+function sumar() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)+parseInt(b)
+
+    document.getElementById('resultado').value=c
+    
+}
+
+function restar() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)-parseInt(b)
+
+    document.getElementById('resultado').value=c
+}
+
+function multiplicacion() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)*parseInt(b)
+
+    document.getElementById('resultado').value=c
+}
+
+function division() {
+    var a = parseInt(document.getElementById("a").value)
+    var b = parseInt(document.getElementById("b").value)
+    
+    var c = parseInt(a)/parseInt(b)
+
+    document.getElementById('resultado').value=c
+>>>>>>> ba31350 (Este es el mas reciente (07/nov/2023))
+}
